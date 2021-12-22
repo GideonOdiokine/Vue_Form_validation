945ea31ed83312c0910a7a2a8680ed1402737a5e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-22 20:18:02 +0100

    added all files

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h2>Gidi Reaction Timer</h2>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showScore" :score="score" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: {
+    Block,
+    Result,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false,
+    };
+  },
+  methods: {
+    start() {
+      this.showScore = false;
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+    endGame(reaction) {
+      this.score = reaction;
+      this.isPlaying = false;
+      this.showScore = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

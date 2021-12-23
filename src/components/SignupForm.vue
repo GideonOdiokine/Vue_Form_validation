36271c7cf7173a738e468d208677ea8da687e140
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-23 11:05:27 +0100

    finished learning how to add user skills to an array

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,10 +5,30 @@
 
     <label> Password: </label>
     <input type="password" required v-model="password" />
+
+    <label>Role </label>
+    <select v-model="role">
+      <option value="developer">Web developer</option>
+      <option value="designer">Web designer</option>
+    </select>
+
+    <labal>Skills: </labal>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill">
+      {{ skill }}
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="isChecked" />
+      <label> Accept terms and condition</label>
+    </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms: {{ isChecked }}</p>
+  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -17,7 +37,21 @@ export default {
     return {
       email: "",
       password: "",
+      role: "",
+      isChecked: false,
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
   },
 };
 </script>
@@ -48,5 +82,23 @@ input {
   border-bottom: 1px solid #ddd;
   color: #555;
   border: none;
+}
+select {
+  display: block;
+  padding: 18px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+.terms {
+  margin: 10px 0;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: inherit;
+  top: 12px;
 }
 </style>
